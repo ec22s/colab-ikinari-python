b9f53a3dfd62717e0dc4482df25e449c13f8be8f
Satoshi Koda <77545144+ec22s@users.noreply.github.com>
2026-01-08 22:31:35 +0900

    Add example of enhance-1

---
+++ day-1/enhance-ex-1.py
@@ -0,0 +1,31 @@
+# 第1回 (6) プログラム変更 (1) 改良例
+print('1から100までの間で好きな数字を一つ思い浮かべて下さい。')
+print('思い浮かべましたか？')
+answer = input()
+if answer != 'yes':
+  print('いったん終了します。思い浮かんだら再実行して下さい。')
+else:
+  print('あなたの考えている数字を7回以内に当ててみましょう。')
+
+  # low: 最小値, high: 最大値
+  low = 1
+  high = 100
+  print(low, high)
+
+  for i in range(7):
+    # low と high が同じならループを抜ける
+    if low == high:
+      break
+
+    # コンピュータの推測値を確認
+    guess = (low + high) // 2
+    print('あなたの数字は', guess, 'より大きいですか？ (yes/no)')
+    answer = input()
+
+    # ユーザの答えにより分岐
+    if answer == 'yes':
+      low = guess + 1
+    else:
+      high = guess
+
+  print('あなたの思い浮かべた数字は', low, 'ですね！')
