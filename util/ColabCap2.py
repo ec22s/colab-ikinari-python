c04c4882211281fc271d30396f1b50b893d01b05
Satoshi Koda <77545144+ec22s@users.noreply.github.com>
2026-06-20 04:17:46 +0900

    Change default of param 'preview' in ColabCap2

--- util/ColabCap2.py
+++ util/ColabCap2.py
@@ -38,7 +38,7 @@ class ColabCap2:
 
   is_opened = False
 
-  def __init__(self, quality=0.8, first_wait_sec=0.25, preview=False):
+  def __init__(self, quality=0.8, first_wait_sec=0.25, preview=True):
     self.quality = quality
     self.first_wait_sec = first_wait_sec
     self.preview = "true" if preview else "false"
