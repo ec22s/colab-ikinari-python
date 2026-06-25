755a95680b92846e7f5cb82319ad1339f99b9076
Satoshi Koda <77545144+ec22s@users.noreply.github.com>
2026-06-25 10:15:40 +0900

    Add util function colab_imshow

---
+++ util/colab_imshow.py
@@ -0,0 +1,25 @@
+from base64 import b64encode
+from google.colab.output import eval_js
+from IPython.display import display, Javascript
+import cv2
+
+def colab_imshow(ext, ndarray):
+  ret, buf = cv2.imencode(f'.{ext}', ndarray)
+  b64 = b64encode(buf.tobytes()).decode()
+
+  display(Javascript("""
+    const IMG_DOM_ID = "imageInOutput";
+    const output = document.querySelector("#output-area");
+    let imgDom = output.querySelector(`#${IMG_DOM_ID}`);
+    if (!imgDom) {
+      imgDom = document.createElement("img");
+      imgDom.id = IMG_DOM_ID;
+      imgDom.style.marginLeft = "1em";
+      output.appendChild(imgDom);
+    }
+
+    function drawImage(ext, b64) {
+      imgDom.src = `data:image/{ext};base64,${b64}`;
+    }
+  """))
+  eval_js(f"""drawImage("{ext}", "{b64}")""")
