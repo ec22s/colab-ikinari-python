cb1561ccceac21046507defea91347816fea12af
Satoshi Koda <77545144+ec22s@users.noreply.github.com>
2026-07-10 11:04:25 +0900

    Update util func colab_imshow to change img size

--- util/colab_imshow.py
+++ util/colab_imshow.py
@@ -3,12 +3,12 @@ from google.colab.output import eval_js
 from IPython.display import display, Javascript
 import cv2
 
-def colab_imshow(ext, ndarray):
+def colab_imshow(ext, ndarray, height=240):
   ret, buf = cv2.imencode(f'.{ext}', ndarray)
   b64 = b64encode(buf.tobytes()).decode()
 
   display(Javascript("""
-    const IMG_DOM_ID = "imageInOutput";
+    const IMG_DOM_ID = "imageOutput";
     const output = document.querySelector("#output-area");
     let imgDom = output.querySelector(`#${IMG_DOM_ID}`);
     if (!imgDom) {
@@ -18,8 +18,9 @@ def colab_imshow(ext, ndarray):
       output.appendChild(imgDom);
     }
 
-    function drawImage(ext, b64) {
+    function drawImage(ext, b64, height) {
+      imgDom.height = height;
       imgDom.src = `data:image/{ext};base64,${b64}`;
     }
   """))
-  eval_js(f"""drawImage("{ext}", "{b64}")""")
+  eval_js(f"""drawImage("{ext}", "{b64}", {height})""")
